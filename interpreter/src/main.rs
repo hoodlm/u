69ec58ab094984f10406915ae4a40300a69ea498
Logan M Hood <hoodlma@gmail.com>
2023-12-31 17:03:49 -0500

    refactor lex/syntax into separate modules

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
 use std::fs;
 
-use regex::Regex;
+
+use crate::lex::lex_analysis;
+use crate::syntax::syntax_analysis;
 
 fn main() {
     println!("\n==== u ====");
@@ -33,163 +35,172 @@ fn infile_from_args() -> String {
     return String::from(&args[1]);
 }
 
-#[derive(Debug, PartialEq, Clone)]
-enum TokenName {
-    ProgramStart,
-    Integer,
-    Plus,
-    Stdout,
-    Unknown,
-}
+mod lex {
+    use regex::Regex;
 
-#[derive(Debug, PartialEq, Clone)]
-struct Token {
-    name: TokenName,
-    value: Option<String>,
-}
-
-fn lex_analysis(input: &String) -> Vec<Token> {
-    let tokens: Vec<Token> = input.split_whitespace().map(|it| to_token(it)).collect();
-
-    let errors: Vec<String> = tokens
-        .iter()
-        .filter(|token| token.name == TokenName::Unknown)
-        .map(|token| format!("Unknown token {:?}", token.value))
-        .collect();
-
-    if errors.is_empty() {
-        return tokens;
-    } else {
-        let message = errors.join("\n");
-        panic!("{}", message);
+    #[derive(Debug, PartialEq, Clone)]
+    pub enum TokenName {
+        ProgramStart,
+        Integer,
+        Plus,
+        Stdout,
+        Unknown,
     }
-}
 
-fn to_token(t: &str) -> Token {
-    let int_regex = Regex::new(r"[0-9]+").unwrap();
+    #[derive(Debug, PartialEq, Clone)]
+    pub struct Token {
+        pub name: TokenName,
+        pub value: Option<String>,
+    }
 
-    let (name, value) = match t {
-        t if int_regex.is_match(t) => (TokenName::Integer, Some(t.to_string())),
-        "+" => (TokenName::Plus, None),
-        "STDOUT" => (TokenName::Stdout, None),
-        _ => (TokenName::Unknown, Some(t.to_string())),
-    };
-    return Token { name, value };
-}
+    pub fn lex_analysis(input: &String) -> Vec<Token> {
+        let tokens: Vec<Token> = input.split_whitespace().map(|it| to_token(it)).collect();
 
-#[derive(Debug, PartialEq, Clone)]
-enum SyntaxTreeKind {
-    ProgramStart,
-    /*
-    Statement,
-    Source,
-    UnaryOp,
-    Sink,
-    */
-}
+        let errors: Vec<String> = tokens
+            .iter()
+            .filter(|token| token.name == TokenName::Unknown)
+            .map(|token| format!("Unknown token {:?}", token.value))
+            .collect();
 
-#[derive(Debug, PartialEq, Clone)]
-struct SyntaxTree {
-    kind: SyntaxTreeKind,
-    parent: Option<Box<SyntaxTreeKind>>,
-    children: Vec<SyntaxTree>,
-}
-
-impl SyntaxTree {
-    fn root() -> Self {
-        SyntaxTree {
-            kind: SyntaxTreeKind::ProgramStart,
-            parent: None,
-            children: Vec::new(),
+        if errors.is_empty() {
+            return tokens;
+        } else {
+            let message = errors.join("\n");
+            panic!("{}", message);
         }
     }
 
-    fn add_child(&mut self, child: SyntaxTree) {
-        self.children.push(child);
-    }
-}
+    fn to_token(t: &str) -> Token {
+        let int_regex = Regex::new(r"[0-9]+").unwrap();
 
-#[derive(Debug, PartialEq)]
-struct SyntaxParser {
-    state: SyntaxParserState,
-}
-
-#[derive(Debug, PartialEq)]
-enum SyntaxParserState {
-    GetNextLine,
-    BuildingStatement,
-    Complete,
-}
-
-impl SyntaxParser {
-    fn new() -> Self {
-        return SyntaxParser {
-            state: SyntaxParserState::GetNextLine,
+        let (name, value) = match t {
+            t if int_regex.is_match(t) => (TokenName::Integer, Some(t.to_string())),
+            "+" => (TokenName::Plus, None),
+            "STDOUT" => (TokenName::Stdout, None),
+            _ => (TokenName::Unknown, Some(t.to_string())),
         };
-    }
-
-    fn is_done(&self) -> bool {
-        self.state == SyntaxParserState::Complete
-    }
-
-    fn handle_next(&mut self, token: &Option<&Token>, tree: &mut SyntaxTree) {
-        match &self.state {
-            SyntaxParserState::GetNextLine => self.get_next_line_or_end_program(token, tree),
-            SyntaxParserState::BuildingStatement => self.append_to_statement(token, tree),
-            _ => panic!("state not implemented"),
-        }
-    }
-
-    fn get_next_line_or_end_program(&mut self, token: &Option<&Token>, tree: &mut SyntaxTree) {
-        match token {
-            // None == end of file
-            None => self.state = SyntaxParserState::Complete,
-            Some(token) => self.start_next_line(token, tree),
-        }
-    }
-
-    fn start_next_line(&mut self, token: &Token, tree: &mut SyntaxTree) {
-        match token.name {
-            TokenName::Integer => {
-                println!(
-                    "Would start new Statement with integer value {:?}",
-                    token.value
-                );
-                println!("Transition state to BuildingStatement");
-                self.state = SyntaxParserState::BuildingStatement;
-            }
-            _ => println!("NOOP for {:?}", token),
-        }
-    }
-
-    fn append_to_statement(&mut self, token: &Option<&Token>, tree: &mut SyntaxTree) {
-        match token {
-            None => panic!("Unexpected end of file"),
-            Some(token) => match token.name {
-                TokenName::Plus => {
-                    println!("Would append {:?} to current statement", token.name);
-                }
-                TokenName::Stdout => {
-                    println!("Would append {:?} to current statement", token.name);
-                    println!("Statement complete; Transition state to GetNextLine");
-                    self.state = SyntaxParserState::GetNextLine;
-                }
-                _ => {
-                    panic!("Unexpected token!");
-                }
-            },
-        }
+        return Token { name, value };
     }
 }
 
-fn syntax_analysis(input: &Vec<Token>) -> SyntaxTree {
-    let mut tree = SyntaxTree::root();
-    let mut syntax_parser = SyntaxParser::new();
-    let mut tokens = input.iter();
+mod syntax {
 
-    while !syntax_parser.is_done() {
-        let token = tokens.next();
-        syntax_parser.handle_next(&token, &mut tree);
+    use crate::lex::{Token, TokenName};
+
+    #[derive(Debug, PartialEq, Clone)]
+    enum SyntaxTreeKind {
+        ProgramStart,
+        /*
+        Statement,
+        Source,
+        UnaryOp,
+        Sink,
+        */
     }
-    return tree;
+
+    #[derive(Debug, PartialEq, Clone)]
+    pub struct SyntaxTree {
+        kind: SyntaxTreeKind,
+        parent: Option<Box<SyntaxTreeKind>>,
+        children: Vec<SyntaxTree>,
+    }
+
+    impl SyntaxTree {
+        fn root() -> Self {
+            SyntaxTree {
+                kind: SyntaxTreeKind::ProgramStart,
+                parent: None,
+                children: Vec::new(),
+            }
+        }
+
+        fn add_child(&mut self, child: SyntaxTree) {
+            self.children.push(child);
+        }
+    }
+
+    #[derive(Debug, PartialEq)]
+    struct SyntaxParser {
+        state: SyntaxParserState,
+    }
+
+    #[derive(Debug, PartialEq)]
+    enum SyntaxParserState {
+        GetNextLine,
+        BuildingStatement,
+        Complete,
+    }
+
+    impl SyntaxParser {
+        fn new() -> Self {
+            return SyntaxParser {
+                state: SyntaxParserState::GetNextLine,
+            };
+        }
+
+        fn is_done(&self) -> bool {
+            self.state == SyntaxParserState::Complete
+        }
+
+        fn handle_next(&mut self, token: &Option<&Token>, tree: &mut SyntaxTree) {
+            match &self.state {
+                SyntaxParserState::GetNextLine => self.get_next_line_or_end_program(token, tree),
+                SyntaxParserState::BuildingStatement => self.append_to_statement(token, tree),
+                _ => panic!("state not implemented"),
+            }
+        }
+
+        fn get_next_line_or_end_program(&mut self, token: &Option<&Token>, tree: &mut SyntaxTree) {
+            match token {
+                // None == end of file
+                None => self.state = SyntaxParserState::Complete,
+                Some(token) => self.start_next_line(token, tree),
+            }
+        }
+
+        fn start_next_line(&mut self, token: &Token, tree: &mut SyntaxTree) {
+            match token.name {
+                TokenName::Integer => {
+                    println!(
+                        "Would start new Statement with integer value {:?}",
+                        token.value
+                    );
+                    println!("Transition state to BuildingStatement");
+                    self.state = SyntaxParserState::BuildingStatement;
+                }
+                _ => println!("NOOP for {:?}", token),
+            }
+        }
+
+        fn append_to_statement(&mut self, token: &Option<&Token>, tree: &mut SyntaxTree) {
+            match token {
+                None => panic!("Unexpected end of file"),
+                Some(token) => match token.name {
+                    TokenName::Plus => {
+                        println!("Would append {:?} to current statement", token.name);
+                    }
+                    TokenName::Stdout => {
+                        println!("Would append {:?} to current statement", token.name);
+                        println!("Statement complete; Transition state to GetNextLine");
+                        self.state = SyntaxParserState::GetNextLine;
+                    }
+                    _ => {
+                        panic!("Unexpected token!");
+                    }
+                },
+            }
+        }
+    }
+
+    pub fn syntax_analysis(input: &Vec<Token>) -> SyntaxTree {
+        let mut tree = SyntaxTree::root();
+        let mut syntax_parser = SyntaxParser::new();
+        let mut tokens = input.iter();
+
+        while !syntax_parser.is_done() {
+            let token = tokens.next();
+            syntax_parser.handle_next(&token, &mut tree);
+        }
+        return tree;
+    }
 }
