b411d28f12363169de2125ce373ed73ebfddc216
Logan M Hood <hoodlma@gmail.com>
2023-12-31 13:07:34 -0500

    Lexer with support for a few basic tokens
    ==== u ====
    Interpreting /home/logan/Code/u/examples/2plus3.u
    2 + + + STDOUT
    4 + + + STDOUT
    
    ...
    Token { name: Integer, value: Some("2") }
    Token { name: Plus, value: None }
    Token { name: Plus, value: None }
    Token { name: Plus, value: None }
    Token { name: Stdout, value: None }
    Token { name: Integer, value: Some("4") }
    Token { name: Plus, value: None }
    Token { name: Plus, value: None }
    Token { name: Plus, value: None }
    Token { name: Stdout, value: None }
    
    ==== u ====
    Interpreting /home/logan/Code/u/examples/invalid.u
    2 + + + STDOUT
    3 + BADTOKEN + STDOUT
    4 + + + STDOUT
    
    ...
    thread 'main' panicked at src/main.rs:56:9:
    Unknown token Some("BADTOKEN")

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,14 +1,22 @@
 use std::env;
 use std::fs;
 
+use regex::Regex;
+
 fn main() {
+    println!("\n==== u ====");
     let filename = infile_from_args();
     let input = fs::read_to_string(&filename).unwrap();
 
+    println!("Interpreting {}", filename);
+    println!("{}", input);
+    println!("...");
+
     let tokens = lex_analysis(&input);
 
-    println!("{}", input);
-    println!("{:?}", tokens);
+    tokens.iter().for_each(|t| {
+        println!("{:?}", t);
+    });
 }
 
 fn infile_from_args() -> String {
@@ -21,6 +29,9 @@ fn infile_from_args() -> String {
 
 #[derive(Debug, PartialEq)]
 enum TokenName {
+    Integer,
+    Plus,
+    Stdout,
     Unknown,
 }
 
@@ -46,9 +57,14 @@ fn lex_analysis(input: &String) -> Vec<Token> {
     }
 }
 
-fn to_token(token_value_raw: &str) -> Token {
-    return Token {
-        name: TokenName::Unknown,
-        value: Some(token_value_raw.to_string())
+fn to_token(t: &str) -> Token {
+    let int_regex = Regex::new(r"[0-9]+").unwrap();
+
+    let (name, value) = match t {
+        t if int_regex.is_match(t) => (TokenName::Integer, Some(t.to_string())),
+        "+" => (TokenName::Plus, None),
+        "STDOUT" => (TokenName::Stdout, None),
+        _ => (TokenName::Unknown, Some(t.to_string())),
     };
+    return Token { name, value };
 }
