a6761680b8d1be2abdd1b89ef0d778b89ca71154
Logan M Hood <hoodlma@gmail.com>
2023-12-31 12:40:10 -0500

    Barebones lexer that panics on every token
    $ cargo run ../examples/2plus3.u
      Running `target/debug/u ../examples/2plus3.u`
    thread 'main' panicked at src/main.rs:45:9:
    Unknown token Some("2")
    Unknown token Some("+")
    Unknown token Some("+")
    Unknown token Some("+")
    Unknown token Some("PRINTLN;")

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,3 +1,54 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = infile_from_args();
+    let input = fs::read_to_string(&filename).unwrap();
+
+    let tokens = lex_analysis(&input);
+
+    println!("{}", input);
+    println!("{:?}", tokens);
+}
+
+fn infile_from_args() -> String {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("usage: u hello.u");
+    }
+    return String::from(&args[1]);
+}
+
+#[derive(Debug, PartialEq)]
+enum TokenName {
+    Unknown,
+}
+
+#[derive(Debug)]
+struct Token {
+    name: TokenName,
+    value: Option<String>,
+}
+
+fn lex_analysis(input: &String) -> Vec<Token> {
+    let tokens: Vec<Token> = input.split_whitespace().map(|it| to_token(it)).collect();
+
+    let errors: Vec<String> = tokens.iter()
+        .filter(|token| token.name == TokenName::Unknown)
+        .map(|token| format!("Unknown token {:?}", token.value))
+        .collect();
+
+    if errors.is_empty() {
+        return tokens;
+    } else {
+        let message = errors.join("\n");
+        panic!("{}", message);
+    }
+}
+
+fn to_token(token_value_raw: &str) -> Token {
+    return Token {
+        name: TokenName::Unknown,
+        value: Some(token_value_raw.to_string())
+    };
 }
